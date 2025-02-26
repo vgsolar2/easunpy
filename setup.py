7291a93ec9309c24dd2dca5818736b9d859fbc88
vgsolar2 <vgsolar2@proton.me>
2025-02-26 23:53:54 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easunpy',
-    version='0.1.24',
+    version='0.1.25',
     description='A tool for monitoring Easun ISolar inverters',
     author_email='vgsolar2@proton.me',
     url='https://github.com/vgsolar2/easunpy',
